288deb0d0ad05d431b75afa18bcd50c5ad8be4dc
nikdesigns <nitinkaushikdesigns@gmail.com>
2021-04-19 14:28:15 +0530

    Form checkbox added

--- src/Form.js
+++ src/Form.js
@@ -2,21 +2,36 @@ import { useState } from 'react';
 import './form.css';
 
 const Form = () => {
-  const [formState, setFormState] = useState({
+  const initialState = {
     firstName: '',
     lastName: '',
-  });
+    biography: '',
+    transport: '',
+    agree: false,
+  };
+
+  const [formState, setFormState] = useState(initialState);
 
   const onChangeHandler = (e) => {
+    const value =
+      e.target.type === 'checkbox' ? e.target.checked : e.target.value;
     setFormState({
       ...formState,
-      [e.target.name]: e.target.value,
+      [e.target.name]: value,
     });
   };
 
+  const onClickHandler = () => {
+    setFormState(initialState);
+  };
+
+  const onSubmitHandler = (e) => {
+    e.preventDefault();
+    console.log(formState);
+  };
+
   return (
-    <form>
-      <span>{`Your name is ${formState.firstName} ${formState.lastName}`}</span>
+    <form onSubmit={onSubmitHandler}>
       <label htmlFor="firstName"> First name </label>
       <input
         id="firstName"
@@ -31,6 +46,39 @@ const Form = () => {
         onChange={onChangeHandler}
         value={formState.lastName}
       />
+      <label htmlFor="biography">Biography</label>
+      <textarea
+        id="biography"
+        name="biography"
+        rows="10"
+        onChange={onChangeHandler}
+        value={formState.biography}
+      />
+      <label htmlFor="transport">Preferred transport</label>
+      <select
+        id="transport"
+        name="transport"
+        onChange={onChangeHandler}
+        value={formState.transport}
+      >
+        <option>None Selected</option>
+        <option value="planes">Planes</option>
+        <option value="trains">Trains</option>
+        <option value="cars">Cars</option>
+        <option value="boats">Boats</option>
+      </select>
+      <label htmlFor="agree">I agree to the TM*.</label>
+      <input
+        type="checkbox"
+        id="agree"
+        name="agree"
+        onChange={onChangeHandler}
+        checked={formState.agree}
+      />
+      <button type="submit">Save</button>
+      <button type="button" onClick={onClickHandler}>
+        Clear
+      </button>
     </form>
   );
 };
