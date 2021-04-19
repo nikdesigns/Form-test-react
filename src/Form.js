fd363499ae33411fe7841f3940a33a7c18926b95
nikdesigns <nitinkaushikdesigns@gmail.com>
2021-04-19 12:30:36 +0530

    Form Test

---
+++ src/Form.js
@@ -0,0 +1,12 @@
+import './form.css';
+
+const Form = () => {
+  return (
+    <form>
+      <label htmlFor="firstName"> First name </label>
+      <input id="firstName" />
+    </form>
+  );
+};
+
+export default Form;
